58316ed02744d1b97697c9b1593cfb7932743be7
David Codner <dccodner24@gmail.com>
2022-12-19 21:36:24 -0600

    Models written, database properly seeding on startup.

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// Create connection to the DB via Sequelize. Use .env to prepare for connection to Heroku database.
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
